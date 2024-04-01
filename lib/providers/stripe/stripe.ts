c6e2535ea7012de4c8146a3ef8ed9c233053e0d0
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2024-04-01 00:01:18 -0700

    test live stripe

--- lib/providers/stripe/stripe.ts
+++ lib/providers/stripe/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? "",
+  process.env.STRIPE_SECRET_KEY_LIVE as string, 
   {
     // https://github.com/stripe/stripe-node#configuration
     apiVersion: "2023-10-16",
