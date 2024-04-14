0a28542cba209f5a653a49bfdd8b773b269f1b90
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2024-04-14 14:06:42 -0700

    Update LinkModal.tsx

--- ui/Components/LinkModal.tsx
+++ ui/Components/LinkModal.tsx
@@ -14,16 +14,6 @@ const spotifyUrlRegex: { [key: string]: RegExp } = {
   track: /https:\/\/open.spotify.com\/track\/([a-zA-Z0-9]+)/,
   playlist: /https:\/\/open.spotify.com\/playlist\/([a-zA-Z0-9]+)/,
   album: /https:\/\/open.spotify.com\/album\/([a-zA-Z0-9]+)/,
-
-};
-const getTypeAndId = (url: string) => {
-  for (const [type, regex] of Object.entries(spotifyUrlRegex)) {
-    const match = url.match(regex);
-    if (match && match[1]) {
-      return { type, id: match[1] };
-    }
-  }
-  return null; // Invalid URL or unsupported type
 };
 
 function extractTrackId(url: string, type: string) {
@@ -75,86 +65,82 @@ export const ButtonGroup = ({ song, links }: { song: any; links?: any }) => {
       <div className="scale-75 mx-auto my-4">
         {links && <SocialLinkGroup links={links} />}
       </div>
-      {song?.spotifyUrl && song.type === 'ep' && (
+      {song?.spotifyUrl && song.type === "ep" && (
         <iframe
           className="mb-4"
-          src={`https://open.spotify.com/embed/album/${extractTrackId(song.spotifyUrl, "album")}?utm_source=generator`}
+          src={`https://open.spotify.com/embed/album/${extractTrackId(
+            song.spotifyUrl,
+            "album"
+          )}?utm_source=generator`}
           width="100%"
           height="152"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
       )}
-      {song?.spotifyUrl && song.type === 'single' && (
+      {song?.spotifyUrl && song.type === "single" && (
         <iframe
           className="mb-4"
-          src={`https://open.spotify.com/embed/track/${extractTrackId(song.spotifyUrl, "track")}?utm_source=generator`}
+          src={`https://open.spotify.com/embed/track/${extractTrackId(
+            song.spotifyUrl,
+            "track"
+          )}?utm_source=generator`}
           width="100%"
           height="152"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
       )}
-      {song?.spotifyUrl && (
-        <Link className="hidden" target="_blank" href={song.spotifyUrl}>
-          <div className="text-white text-center w-full bg-green-700 hover:bg-green-600   font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2  focus:outline-none ">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <FaSpotify />
-              <p className="font-owners font-bold">Listen on Spotify</p>{" "}
-            </div>
-          </div>
-        </Link>
-      )}
-      {song?.amazonUrl && (
-        <Link target="_blank" href={song.amazonUrl}>
-          <div className="text-white text-center w-full    font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-sky-600 hover:bg-sky-500 focus:outline-none ">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <FaAmazon />
-              <p className="font-owners font-bold">Listen on Amazon Music</p>{" "}
-            </div>
-          </div>
-        </Link>
-      )}
-      {song?.youtubeUrl && (
-        <Link target="_blank" href={song.youtubeUrl}>
-          <div className="text-white text-center w-full    font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-red-600 hover:bg-red-500 focus:outline-none ">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <FaYoutube />
-              <p className="font-owners font-bold">Listen on Youtube</p>{" "}
-            </div>
-          </div>
-        </Link>
-      )}
-      {song?.deezerUrl && (
-        <Link target="_blank" href={song.deezerUrl}>
-          <div className="text-white text-center w-full    font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-indigo-600 hover:bg-indigo-500 focus:outline-none ">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <SiDeezer />
-              <p className="font-owners font-bold">Listen on Deezer</p>{" "}
-            </div>
-          </div>
-        </Link>
-      )}
-      {song?.appleUrl && (
-        <Link target="_blank" href={song.appleUrl}>
-          <div className="text-white text-center w-full    font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-rose-600 hover:bg-rose-500 focus:outline-none ">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <FaMusic />
-              <p className="font-owners font-bold">{"Listen on Apple Music"} </p>{" "}
-            </div>
-          </div>
-        </Link>
+         {song?.appleUrl && (
+        <LinkButton
+          link={song.appleUrl}
+          title={"Listen on Apple Music"}
+          icon={<FaMusic />}
+        />
       )}
       {song?.tidalUrl && (
-        <Link target="_blank" href={song.tidalUrl}>
-          <div className="text-white text-center w-full    font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-black hover:bg-zinc-950 focus:outline-none  border border-zinc-800">
-            <div className="flex mx-auto space-x-2 items-center justify-center">
-              <SiTidal />
-              <p className="font-owners font-bold">Listen on Tidal</p>{" "}
-            </div>
-          </div>
-        </Link>
+        <LinkButton
+          link={song.tidalUrl}
+          title={"Listen on Tidal"}
+          icon={<SiTidal />}
+        />
       )}
+          {song?.youtubeUrl && (
+        <LinkButton
+          link={song.youtubeUrl}
+          title={"Listen on YouTube"}
+          icon={<FaYoutube />}
+        />
+      )}
+      {song?.amazonUrl && (
+        <LinkButton
+          link={song.amazonUrl}
+          title={"Listen on Amazon Music"}
+          icon={<FaAmazon />}
+        />
+      )}
+  
+      {song?.deezerUrl && (
+        <LinkButton
+          link={song.deezerUrl}
+          title={"Listen on Deezer"}
+          icon={<SiDeezer />}
+        />
+      )}
+   
     </div>
+  );
+};
+
+const LinkButton = ({ link, title, icon }: any) => {
+  return (
+    <Link target="_blank" href={link}>
+      <div className="text-white text-center w-full font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 bg-black hover:bg-zinc-950 focus:outline-none  border border-zinc-800">
+        <div className="flex mx-auto space-x-2 items-center justify-center">
+          {icon}
+          <p className="font-owners font-bold">{title}</p>{" "}
+        </div>
+      </div>
+    </Link>
   );
 };
